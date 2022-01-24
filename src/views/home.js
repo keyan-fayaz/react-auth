f9a5a65c84d2a476595331bd3154b8cdd5d9bfb6
keyanfayaz <keyan.fayaz@gmail.com>
2022-01-24 14:33:58 -0500

    upload working files

---
+++ src/views/home.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+
+import { Hero, Content } from "../components";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <Hero />
+        <hr />
+        <Content />
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
